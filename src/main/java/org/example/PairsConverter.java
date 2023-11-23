4066c40c1193072f0eb4fa19e02c74de57bae485
rizvan <rizvangaripov@gmail.com>
2023-11-23 17:34:46 +0300

    7

--- src/main/java/org/example/PairsConverter.java
+++ src/main/java/org/example/PairsConverter.java
@@ -10,7 +10,7 @@ public class PairsConverter {
     public static void main(String[] s) {
         try {
             List<String> pairCURRENCY_RUB = new ArrayList<>();
-            List<String> pairUSD_CURRENCY = Files.readAllLines(Paths.get("D:\\IntelliJ IDEA projects\\SimpleProject\\src\\main\\java\\org\\example\\USDJPY_230714_231114.csv"));
+            List<String> pairUSD_CURRENCY = Files.readAllLines(Paths.get("D:\\IntelliJ IDEA projects\\SimpleProject\\src\\main\\java\\org\\example\\USDUZS_14072023_14112023.csv"));
             List<String> pairUSD_RUB = Files.readAllLines(Paths.get("D:\\IntelliJ IDEA projects\\SimpleProject\\src\\main\\java\\org\\example\\USDRUB_230714_231114.csv"));
 
             pairCURRENCY_RUB.add("<TICKER>;<PER>;<DATE>;<TIME>;<OPEN>;<HIGH>;<LOW>;<CLOSE>;<VOL>");
@@ -21,12 +21,12 @@ public class PairsConverter {
 
                 double open_ar = Double.parseDouble(p_er[4]) / Double.parseDouble(p_ey[4]);
                 double close_ar = Double.parseDouble(p_er[7]) / Double.parseDouble(p_ey[7]);
-                pairCURRENCY_RUB.add("CADRUB;D;"+p_er[2] +";0;" + String.format("%.7f",open_ar).replaceAll(",", ".")+";"+
+                pairCURRENCY_RUB.add("UZSRUB;D;"+p_er[2] +";0;" + String.format("%.7f",open_ar).replaceAll(",", ".")+";"+
                         ";"+
                         ";"+
                         String.format("%.7f",close_ar).replaceAll(",", ".")+";");
 
-                Files.write(Paths.get("JPYRUB_230714_231114.csv"),pairCURRENCY_RUB);
+                Files.write(Paths.get("UZSRUB_230714_231114.csv"),pairCURRENCY_RUB);
             }
         } catch (IOException e) {
             e.printStackTrace();
