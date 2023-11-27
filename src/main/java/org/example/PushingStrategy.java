3de00eff50bd389d122e52a92fc97a97e22e317f
rizvan <rizvangaripov@gmail.com>
2023-11-27 15:02:17 +0300

    delete files

--- src/main/java/org/example/PushingStrategy.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public class PushingStrategy {
-    String json = "{\"name\":\"Гарипов Ризван Русланович, 11-004\",\"currency1\":\"CAD\",\"currency2\":\"JPY\"," +
-            "\"currency3\":\"USD\",\"currency4\":\"UZS\",\"strategy\":\"s1\"}";
-}
